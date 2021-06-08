bd4e28942c51b64c22b9c421b868a77ca86e11de
Lan <admin@highdimen.com>
2021-06-08 19:34:44 -0400

    更新于 06-08 19:34

--- docker/docker/docker-entrypoint.sh
+++ docker/docker/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+crond
+
+echo -e "\n======================== 2. 更新源代码 ========================\n"
 jd update
+echo
 
 crontab /root/jd/config/crontab.list
 
@@ -10,6 +14,8 @@ jd panelon
 
 echo -e "容器启动成功...\n"
 
-crond -f
+if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
+  set -- node "$@"
+fi
 
 exec "$@"
